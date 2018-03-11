b15cc535674d06e9e0c58ed32eef59e3b58c40a9
Anonymous Applicant <anonymous@example.com>
2018-03-11 15:42:46 -0400

    Show address on search results page

--- src/my_exercise/search.clj
+++ src/my_exercise/search.clj
@@ -1,5 +1,7 @@
 (ns my-exercise.search
-  (:require [hiccup.page :refer [html5]]))
+  (:require
+    [clojure.walk :refer [keywordize-keys]]
+    [hiccup.page :refer [html5]]))
 
 (defn header [_]
   [:head
@@ -9,16 +11,40 @@
    [:title "My Elections"]
    [:link {:rel "stylesheet" :href "default.css"}]])
 
-(defn getting-started [_]
+(defn request->address
+  "Take a ring request from a /search POST and pull out address fields
+  as keywords"
+  [request]
+  (-> request
+    :form-params
+    (select-keys ["street" "street-2" "city" "state" "zip"])
+    keywordize-keys))
+
+
+;; TODO: Handle missing fields, add CSS
+(defn format-address
+  "Given an address, format it as hiccup HTML"
+  [{:keys [street street-2 city state zip]}]
   [:div
-   [:h1 "My Elections"]
-   [:p "The following elections were found in your area:"]
+   [:div street]
+   [:div street-2]
+   [:div (str city ", " state " " zip)]])
+
+(defn explanation [request]
+  (let [address (request->address request)]
+    [:div
+     [:h1 "My Elections"]
+     [:p "The following upcoming elections were found near"]
+     (format-address address)]))
+
+(defn election-results [_]
+  [:div
    [:ul
     [:li "Some Election"]
     [:li "Perhaps another election"]]])
 
-
 (defn handle-post [request]
   (html5
     (header request)
-    (getting-started request)))
+    (explanation request)
+    (election-results request)))
--- test/my_exercise/search_test.clj
+++ test/my_exercise/search_test.clj
@@ -1,1 +1,17 @@
-(ns my-exercise.search-test)
+(ns my-exercise.search-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [my-exercise.search :as search]))
+
+
+(deftest format-address
+  (testing "address with all fields"
+    (is (= (search/format-address
+             {:street "20 Jay St"
+              :street-2 "Suite 824"
+              :city "Brooklyn"
+              :state "NY"
+              :zip "11201"})
+          [:div
+           [:div "20 Jay St"]
+           [:div "Suite 824"]
+           [:div "Brooklyn, NY 11201"]]))))
