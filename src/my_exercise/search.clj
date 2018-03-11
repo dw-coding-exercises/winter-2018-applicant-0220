997de946204867c15cfd8a9f08c12ecd98ff7475
Anonymous Applicant <anonymous@example.com>
2018-03-11 16:34:25 -0400

    Add work-in-progress code for calling API

--- src/my_exercise/search.clj
+++ src/my_exercise/search.clj
@@ -2,7 +2,8 @@
   (:require
     [clojure.string :as string]
     [clojure.walk :refer [keywordize-keys]]
-    [hiccup.page :refer [html5]]))
+    [hiccup.page :refer [html5]]
+    [clj-http.client :as client]))
 
 (defn header [_]
   [:head
@@ -33,6 +34,44 @@
                string/lower-case
                (string/replace #" " "_"))]
     (str state-ocd "/place:" city)))
+
+;;;;;;;;;;;;;;;;
+;; Sketchy Code
+;;
+;; This part's a bit rough, but shows the outline of what's coming next.
+;; Derive OCD's from address, combine them into a request URL, and call the API.
+;;;;;;;;;;;;;;;;
+
+;; TODO: test
+(defn address->ocds
+  "Given an address, return a collection of OCD's"
+  [address]
+  (cond-> []
+    (seq (:state address))
+    (conj (->state-ocd address))
+
+    (and
+      (seq (:state address))
+      (seq (:city address)))
+    (conj (->place-ocd address))))
+
+(def api-base-url "https://api.turbovote.org/elections/upcoming?district-divisions=")
+
+;; TODO: building the request URL might be better as its own function, separate from the request
+(defn request-elections-for-ocds
+  [ocds]
+  (let [request-url (str api-base-url (string/join "," ocds))]
+    (client/get request-url)))
+
+;; TODO: handle case of no OCD's derived
+(defn request-elections-for-address
+  [address]
+  (let [ocds (address->ocds address)]
+    (request-elections-for-ocds ocds)))
+
+;;;;;;;;;;;;;;;;
+;; /Sketchy Code
+;;;;;;;;;;;;;;;;
 
 ;; TODO: Handle missing fields, add CSS
 (defn format-address
